b88f1f2d70fdd99b854b13bbc6ffbf8069079f26
martinalonsonu <martin99huacho@gmail.com>
2023-05-25 08:47:35 -0500

    backend complete

--- api/src/controllers/activitiesControler.js
+++ api/src/controllers/activitiesControler.js
@@ -1,20 +1,38 @@
 const { Country, Activity } = require('../db')
 
 const postActivities = async (name, difficulty, duration, season, countries) => {
-
-    const newActivity = await Activity.create({
-        name,
-        difficulty,
-        duration,
-        season,
-    });
-    const country = await Country.findAll({
-        where: {
-            name: countries,
-        },
-    });
-    newActivity.addCountry(country);
-    return newActivity;
+    try {
+        const newActivity = await Activity.create({
+            name,
+            difficulty,
+            duration,
+            season,
+        });
+        const country = await Country.findAll({
+            where: {
+                name: countries,
+            },
+        });
+        newActivity.addCountry(country);
+        return newActivity;
+    } catch (error) {
+        return { error: error.message }
+    }
 };
 
-module.exports = { postActivities }
+const getActivities = async () => {
+    try {
+        const activities = await Activity.findAll({
+            attributes: ['id', 'name', 'difficulty', 'duration', 'season'],
+            include: [{
+                model: Country,
+                attributes: ['id', 'name', 'flag', 'continent', 'capital', 'subregion', 'area', 'population']
+            }]
+        })
+        return activities;
+    } catch (error) {
+        return { error: error.message }
+    }
+}
+
+module.exports = { postActivities, getActivities }
--- api/src/routes/activitiesRouter.js
+++ api/src/routes/activitiesRouter.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { postActivities } = require('../controllers/activitiesControler')
+const { postActivities, getActivities } = require('../controllers/activitiesControler')
 
 const router = express.Router();
 
@@ -10,6 +10,15 @@ router.post('/', async (req, res) => {
         res.status(200).json(newActivity);
     } catch (error) {
         res.status(507).send("No se pudo almacenar la información");
+    }
+})
+
+router.get('/', async (req, res) => {
+    try {
+        const activities = await getActivities();
+        activities ? res.status(200).json(activities) : res.status(404).json({ error: 'No se encontraron actividades' })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
 })
 
