fcd378cb32730611431a88b789984b532d47c862
martinalonsonu <martin99huacho@gmail.com>
2023-06-19 22:49:40 -0500

    deploy

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -4,18 +4,18 @@ import style from "./Landing.module.css";
 import mundo from "../../images/mundo.png";
 
 function Landing() {
-  return (
-    <div className={style.container}>
-      <div className={style.welcome}>
-        <img src={mundo} alt="imagenMundo" />
-        <p>Welcome to</p>
-        <h1>WorldApp</h1>
-        <Link to="/home">
-          <button>INICIAR</button>
-        </Link>
-      </div>
-    </div>
-  );
+    return (
+        <div className={style.container}>
+            <div className={style.welcome}>
+                <img src={mundo} alt="imagenMundo" />
+                <p>Welcome to</p>
+                <h1>WorldApp</h1>
+                <Link to="/home">
+                    <button>START!</button>
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default Landing;
